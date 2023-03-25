e38d731d836d72f9404deda3f7fb2e7404ef94a4
Artur Melo <arturmelofavero0504@gmail.com>
2023-03-25 15:11:03 -0300

    Exercício com stream

---
+++ src/model/entities/Employee.java
@@ -0,0 +1,44 @@
+package model.entities;
+
+public class Employee {
+
+	private String name;
+	private String email;
+	private Double salary;
+
+	public Employee(String name, String email, Double salary) {
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getsalary() {
+		return salary;
+	}
+
+	public void setsalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name = " + name + ", email = " + email + ", salary = " + salary + "]";
+	}
+
+}
